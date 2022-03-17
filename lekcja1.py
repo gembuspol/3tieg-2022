bf9afa10e1266af625d6500a624bc0b06c1614e0
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-03-17 15:02:39 +0100

    zmiana kolor
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -4,9 +4,13 @@ import waz
 import jablko
 
 #zmienne globalne
-kolorWaz1=(255,255,0)
-def zmianaKolorWaz1(self,kolor):
-    kolorWaz1=kolor
+
+obiektWaz1=waz.Waz()
+obiektWaz2=waz.Waz()
+def zmianaKolorWaz1(kolor):
+    obiektWaz1.ustawKolor(kolor)
+def zmianaKolorWaz2(kolor):
+    obiektWaz2.ustawKolor(kolor)
 
 def main():
     pygame.init()
@@ -15,9 +19,10 @@ def main():
     run=True
     iloscJablek=3
     #wywołanie klasy waz
-    obiektWaz1=waz.Waz()
-    obiektWaz2=waz.Waz()
-    obiektWaz1.ustawKolor(kolorWaz1)
+    
+    
+    
+
     #tworzenie kilku jablek
     obiektJablko=[]
     for nrJablko in range(0,iloscJablek):
--- start.py
+++ start.py
@@ -1,15 +1,20 @@
+from matplotlib.widgets import Widget
 import pygame
 import pygame_menu
 import lekcja1
 
 def wlaczGre():
     lekcja1.main()
+def zmienKolorWaz1(value):
+    lekcja1.zmianaKolorWaz1(value)
 def main():
     pygame.init()
     oknoMenu=pygame.display.set_mode((500,500))
     pygame.display.set_caption("Wąż")
     menu=pygame_menu.Menu("Gra Snake 3TIEG",500,500,theme=pygame_menu.themes.THEME_DARK)
     menu.add.button('Włącz grę',wlaczGre,background_color=(0,0,255))
+    menu.add.color_input('Kolor Wąż 1:','rgb',default=(100,100,255),onreturn=zmienKolorWaz1)
+    #menu.add.button('Zapisz kolor Wąż 1')
     menu.mainloop(oknoMenu)
 
 main()
