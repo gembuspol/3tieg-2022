88ab6e412d8d816049dc0221acb164d0ed6ec579
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-04-11 15:54:03 +0200

    zadanie
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- generowanieHasla/skrypt.js
+++ generowanieHasla/skrypt.js
@@ -1,17 +1,27 @@
 var iloscZn=8;
 var iloscCyfr=0;
+var iloscMaleL=0;
+var iloscWolnychZn=0;
 
 function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value;
-    document.getElementById("iloscCyfr").max=+iloscZn;
-    document.getElementById("iloscMaleL").max=+iloscZn;
+    
+    ustawWolne();
 }
 
 function wybCyfr(){
     iloscCyfr=document.getElementById("iloscCyfr").value;
     document.getElementById("wybCyfr").value=iloscCyfr;
+    ustawWolne();
 }
 function wybMaleL(){
-    iloscCyfr=document.getElementById("iloscMaleL").value;
-    document.getElementById("wybMaleL").value=iloscCyfr;
+    iloscMaleL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne();
+}
+function ustawWolne(){
+    iloscWolnychZn=+iloscZn-iloscCyfr-iloscMaleL;
+    document.getElementById("wynik").value=iloscWolnychZn;
+    document.getElementById("iloscCyfr").max=+iloscCyfr+ +iloscWolnychZn;
+    document.getElementById("iloscMaleL").max=+iloscMaleL+ +iloscWolnychZn;
 }
