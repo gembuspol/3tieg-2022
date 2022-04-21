9061f37b4ba45f2f4d82f443f846d878a138d6e9
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-04-21 15:28:40 +0200

    4 range
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- generowanieHasla/skrypt.js
+++ generowanieHasla/skrypt.js
@@ -2,6 +2,8 @@ var iloscZn=8;
 var iloscCyfr=0;
 var iloscMaleL=0;
 var iloscWolnychZn=0;
+var iloscDuzeL=0;
+var iloscSpec=0;
 
 function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value;
@@ -19,9 +21,21 @@ function wybMaleL(){
     document.getElementById("wybMaleL").value=iloscMaleL;
     ustawWolne();
 }
+function wybDuzeL(){
+    iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne();
+}
+function wybSpec(){
+    iloscSpec=document.getElementById("iloscSpec").value;
+    document.getElementById("wybSpec").value=iloscSpec;
+    ustawWolne();
+}
 function ustawWolne(){
-    iloscWolnychZn=+iloscZn-iloscCyfr-iloscMaleL;
+    iloscWolnychZn=+iloscZn-(+iloscCyfr+ +iloscMaleL+ +iloscDuzeL+ +iloscSpec);
     document.getElementById("wynik").value=iloscWolnychZn;
     document.getElementById("iloscCyfr").max=+iloscCyfr+ +iloscWolnychZn;
     document.getElementById("iloscMaleL").max=+iloscMaleL+ +iloscWolnychZn;
+    document.getElementById("iloscDuzeL").max=+iloscDuzeL+ +iloscWolnychZn;
+    document.getElementById("iloscSpec").max=+iloscSpec+ +iloscWolnychZn;
 }
