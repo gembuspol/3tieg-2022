3b726609c7be08edf617c5e960a1cb8965577c43
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-04-22 09:09:53 +0200

    generowanie hasla
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- generowanieHasla/skrypt.js
+++ generowanieHasla/skrypt.js
@@ -42,10 +42,35 @@ function ustawWolne(){
 function generujHaslo(){
     let haslo='';
     const alfabetMale="abcdefghijhklmnoprstuvwxyz";
+    const alfabetDuze='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const znakiSpec='~!@-#$';
 
     //losowanie malych liter
     for(let i=0;i<iloscMaleL;i++){
         haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
     }
-    document.getElementById("test").value=haslo;
+    //losowanie dużych liter
+    for(let i=0;i<iloscDuzeL;i++){
+        haslo+=alfabetDuze[Math.floor(Math.random()*alfabetDuze.length)];
+    }
+    //losowanie znaków specjalnych
+    for(let i=0;i<iloscSpec;i++){
+        haslo+=znakiSpec.charAt(Math.floor(Math.random()*znakiSpec.length));
+    }
+    //losowanie cyfr
+    for(let i=0;i<iloscCyfr;i++){
+        haslo+=Math.floor(Math.random()*10);
+    }
+    document.getElementById("test").value=zmianaKolejnosci(haslo);
+}
+function zmianaKolejnosci(haslo){
+    let wynik=haslo.split('');
+    for(let i=0;i<wynik.length;i++){
+        let los=Math.floor(Math.random()*wynik.length);
+        let tmp=wynik[i];
+        wynik[i]=wynik[los];
+        wynik[los]=tmp;
+    }
+    haslo=wynik.join('');
+    return haslo;
 }
