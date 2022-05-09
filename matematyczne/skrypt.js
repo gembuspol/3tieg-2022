4c9faa1d8c0e51ccbbfaf97cbc768e2e8f8bfe93
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-05-09 14:17:01 +0200

    nww
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- matematyczne/skrypt.js
+++ matematyczne/skrypt.js
@@ -1,4 +1,25 @@
 function euklides(){
     let liczbaA=parseInt(document.getElementById("liczbaA").value)
     let liczbaB=parseInt(document.getElementById("liczbaB").value)
+    while(liczbaA!==liczbaB){
+        if(liczbaA>liczbaB){
+            liczbaA=liczbaA-liczbaB
+        }else{
+            liczbaB=liczbaB-liczbaA
+        }
+    }
+    document.getElementById("euklidesWynik").value=liczbaA
+}
+function euklides2(liczbaA,liczbaB){
+    while(liczbaA!==liczbaB){
+        if(liczbaA>liczbaB){
+            liczbaA=liczbaA-liczbaB
+        }else{
+            liczbaB=liczbaB-liczbaA
+        }
+    }
+    return liczbaA
+}
+function NWW(){
+    
 }
